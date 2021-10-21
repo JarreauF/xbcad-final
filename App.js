b3076988fc4e58dc126d20a5260c67743f6be712
malicke-mahlatsi <69585382+malicke-mahlatsi@users.noreply.github.com>
2021-10-21 18:56:27 +0200

    malicke debug

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation-stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Lungs made a change</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//Screens 
+import {Onboarding, DestinationDetail} from "./screens/"
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer theme={theme}>
+        <Stack.Navigator
+          initialRouteName={'Onboarding'}
+        >
+        {/* Screens */}
+        <Stack.Screen 
+          name="Onboarding"
+          components={Onboarding}
+        />
+
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+export default() =>{
+  return <App/>;
+}
